723471a3031fe8037179e0577e67ac868d2710e7
dshaw0004 <dipankarshaw692@gmail.com>
2023-01-01 21:32:37 +0530

    manifest added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textbox = document.getElementById("input");
 const area = document.getElementById("area");
 const reset = document.querySelector("#btn2");
+const bookAnimation = document.querySelector(".bookAnimation");
 function log() {
 	let input = textbox.value;
 	if (input === "") {
@@ -16,7 +17,7 @@ function log() {
 			clrscr();
 			console.log(data);
 			$("#word").text(input);
-
+			bookAnimation.dataset.visible = "false";
 			// console.log(
 			// 	data.phonetics[0].audio !== " "
 			// 		? data.phonetics[0].audio
@@ -34,9 +35,11 @@ function log() {
 						);
 					}
 					if (m.synonyms.length) {
-						$("#area > div:last-child > .ul").append(`<h5>synonyms</h5>`);
 						$("#area > div:last-child > .ul").append(
 							`<ul class="synonyms"></ul>`
+						);
+						$("#area > div:last-child > .ul > .synonyms").append(
+							`<h5>synonyms</h5>`
 						);
 						for (let synonym of m.synonyms) {
 							$("#area > div:last-child > .ul > .synonyms").append(
@@ -45,9 +48,11 @@ function log() {
 						}
 					}
 					if (m.antonyms.length) {
-						$("#area > div:last-child > .ul").append(`<h5>antonyms</h5>`);
 						$("#area > div:last-child > .ul ").append(
 							`<ul class="antonyms"></ul>`
+						);
+						$("#area > div:last-child > .ul > .antonyms").append(
+							`<h5>antonyms</h5>`
 						);
 						for (let antonym of m.antonyms) {
 							$("#area > div:last-child > .ul > .antonyms").append(
@@ -60,11 +65,18 @@ function log() {
 		});
 }
 
+document.querySelector("form").addEventListener("submit", (e) => {
+	e.preventDefault();
+	log();
+});
+
 reset.addEventListener("click", clrscr);
 
 function clrscr() {
+	bookAnimation.dataset.visible = "true";
+
 	$(".subarea").remove();
 	$("#word").text("");
 }
 
-$("#voice").click(() => document.getElementById("myAudio").play());
+// $("#voice").click(() => document.getElementById("myAudio").play());
